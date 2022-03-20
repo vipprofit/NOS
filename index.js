f45c8d0e0ab8e3c70a3c1fcb8e398ed99abe24cd
vipprofit <101968694+vipprofit@users.noreply.github.com>
2022-03-20 12:04:36 +0300

    Create index.js
    test

---
+++ index.js
@@ -0,0 +1,14 @@
+function block(letter, rate, used, father, code) {
+ this.letter = letter;
+ this.rate = rate;
+ this.used = used;
+ this.father = father;
+ this.code = code;
+ }
+ 
+ function FindCode(block) {
+ 
+  if(tree[block.father].code != '') {
+   block.code = tree[block.father].code + '1';
+   }
+   }
